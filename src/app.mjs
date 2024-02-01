796df0e621a3cf14ab4babac904acf42aa320c61
juansanchez <alzatejuan1980@gmail.com>
2024-02-01 10:27:03 -0500

    se establecion limite de solicitudes a la api

--- src/app.mjs
+++ src/app.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import config from "./config.mjs";
 import obtenerPedidos from "./rutas/pedidos.routes.mjs"
 import cors from "cors"
+import rateLimit from "express-rate-limit";
 
 const app = express();
     
@@ -10,6 +11,14 @@ let port;
 //steting
 //Este caso importo el puerto definido
 app.set(`port`,config.port);
+
+const limit = rateLimit({
+    windowMs: 60 * 60 * 1000, // 1 hora
+    max: 5, // límite de peticiones por hora
+    message: 'Has excedido el límite de peticiones por hora.',
+});
+
+app.use(limit);
 
 app.use(cors());
 //le digo al servidor q acepte formato json
